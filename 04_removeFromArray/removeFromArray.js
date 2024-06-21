f855f0fbfed96d32c0e020bb20f053446153a4c8
Chan Guan Yu <changuanyu2904@gmail.com>
2024-06-21 17:11:32 +0800

    Completed Exercise_04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rest) {
+    return arr.filter(x => removable(x, rest));
 };
+
+function removable(element, arr) {
+    if (arr.includes(element)) {
+        return false;
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
